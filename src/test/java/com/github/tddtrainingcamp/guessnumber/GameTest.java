8adc25a2e1a637f2a23997c08e2137296a70d2cc
WY08271 <wy08271@outlook.com>
2016-01-27 09:34:21 +0800

    add Gametest file

--- src/test/java/com/github/tddtrainingcamp/guessnumber/GameTest.java
+++ src/test/java/com/github/tddtrainingcamp/guessnumber/GameTest.java
@@ -1,8 +1,14 @@
 package com.github.tddtrainingcamp.guessnumber;
 
 import org.junit.Test;
+import org.mockito.InOrder;
 
+import java.io.BufferedReader;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Thoughtworks on 1/26/16.
@@ -10,7 +16,38 @@ import static org.junit.Assert.*;
 public class GameTest {
 
     @Test
-    public void testStart() throws Exception {
+    public void welcome_when_start_game() throws Exception {
 
+        BufferedReader reader = mock(BufferedReader.class);
+        PrintStream out = mock(PrintStream.class);
+        Game game = new Game(reader, out);
+        when(reader.readLine()).thenReturn("");
+
+        game.start();
+
+        InOrder inOrder = inOrder(out);
+        inOrder.verify(out).println("Welcome!");
+        inOrder.verify(out).println("Please input your number(6):");
+    }
+
+    @Test
+    public void congratulation_when_first_try_match() throws Exception {
+        BufferedReader reader = mock(BufferedReader.class);
+        PrintStream out = mock(PrintStream.class);
+        Game game = new Game(reader, out);
+        when(reader.readLine()).thenReturn("1234");
+
+        game.start();
+
+        InOrder inOrder = inOrder(out);
+        inOrder.verify(out).println("Welcome!");
+        inOrder.verify(out).println("Congratulation");
+    }
+
+    @Test
+    public void generate_answer_is_random() throws Exception {
+        String answer1 = AnswerGenerator.genrate();
+        String answer2 = AnswerGenerator.genrate();
+        assertThat(answer1.equals(answer2), is(false));
     }
 }
